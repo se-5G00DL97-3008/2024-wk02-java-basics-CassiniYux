d0a2fe9a24594c9f0c6672540dc999864a3fb0ec
Yushi Tan <119079904+CassiniYux@users.noreply.github.com>
2024-01-18 07:14:58 +0200

    Update B01.java

--- b01/B01.java
+++ b01/B01.java
@@ -1,40 +1,5 @@
 class B01 {
     public static void main(String[] args) {
-        // Declare and initialize integer variable
-        int x = 10;
-
-        // Declare and initialize floating-point variable
-        float y = 5.5f;
-
-        // Declare and initialize a boolean variable
-        boolean flag = true;
-
-        // Declare and initialize a character variable
-        char letter = 'c';
-
-        // Declare and initialize short integer variable
-        short year = 2022;
-
-        // Declare and initialize long integer variable
-        long population = 7000000000L;
-
-        // Declare and initialize byte variable
-        byte num = 100;
-
-        // Declare and initialize double variable
-        double pi = 3.13457599923384753929348;
-
-        // Print the values of the variables
-        System.out.println("x: " + x);
-        System.out.println("y: " + y);
-        System.out.println("flag: " + flag);
-        System.out.println("letter: " + letter);
-        System.out.println("year: " + year);
-        System.out.println("population: " + population);
-        System.out.println("num: " + num);
-        System.out.println("pi: " + pi);
-    }
-}
-
+       
     }
 }
