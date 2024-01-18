261bfc9d31a79579c5e2c2ef72335a7ab0d4b977
Yushi Tan <119079904+CassiniYux@users.noreply.github.com>
2024-01-18 08:20:00 +0200

    Update E04.java

--- e04/E04.java
+++ e04/E04.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 class E04 {
     public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
 
+        // Ask the user to enter a day
+        System.out.println("Enter a day:");
+        int day = scanner.nextInt();
+
+        // Ask the user to enter a month
+        System.out.println("Enter a month:");
+        int month = scanner.nextInt();
+
+        // Check if it's Christmas
+        if (day == 24 && month == 12) {
+            System.out.println("Merry Christmas");
+        } else {
+            System.out.println("No special greeting for this day");
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
