6b940759145f90a4b0ca994c38c553855defc972
Yushi Tan <119079904+CassiniYux@users.noreply.github.com>
2024-01-18 08:44:09 +0200

    Update E08.java

--- e08/E08.java
+++ e08/E08.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 class E08 {
     public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            // Ask the user to enter a number
+            System.out.println("Enter a number (0 to quit):");
+            int number = scanner.nextInt();
+
+            // Check if the number is 0 to quit
+            if (number == 0) {
+                break;
+            }
+
+            // Check if the number is odd or even using modulo %
+            if (number % 2 == 0) {
+                System.out.println("Even number");
+            } else {
+                System.out.println("Odd number");
+            }
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
