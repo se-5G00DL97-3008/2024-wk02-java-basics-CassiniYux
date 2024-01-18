a8f9c565e7c3c84a98ff96ea1f69eb18528eda2d
Yushi Tan <119079904+CassiniYux@users.noreply.github.com>
2024-01-18 07:18:35 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class E02 {
+class E02 {
     public static void main(String[] args) {
         // Create a Scanner object to read input from the user
         Scanner scanner = new Scanner(System.in);
