77d872ad95b87e87bcd1839d5972efe215349b94
Yushi Tan <119079904+CassiniYux@users.noreply.github.com>
2024-01-18 08:18:19 +0200

    Update E03.java

--- e03/E03.java
+++ e03/E03.java
@@ -2,41 +2,24 @@ import java.util.Scanner;
 
 class E03 {
     public static void main(String[] args) {
-       
         // Create a Scanner object to read input
-        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // Ask the user to enter a number
         System.out.println("Enter a number:");
-        double number1 = scanner.nextDouble();
+        int number = scanner.nextInt();
 
         // Check if the number is positive or negative
-        if (number1 > 0) {
+        if (number > 0) {
             System.out.println("Positive number");
-        } else if (number1 < 0) {
+        } else if (number < 0) {
             System.out.println("Negative number");
         } else {
             System.out.println("The number is zero");
         }
-         scanner.close();
- 
-        Scanner scanner2 = new Scanner(System.in);
-        
-        System.out.println("Enter a number:");
-        double number2 = scanner.nextDouble();
 
-        if (number2 > 0) {
-            System.out.println("Positive number");
-        } else if (number2 < 0) {
-            System.out.println("Negative number");
-        } else {
-            System.out.println("The number is zero");
-        }
-        
         // Close the scanner
         scanner.close();
- 
-       
     }
 }
 
