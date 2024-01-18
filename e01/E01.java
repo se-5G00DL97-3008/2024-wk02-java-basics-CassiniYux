750763378a5b42ac9a0a8b4d8faeb87edc634817
Yushi Tan <119079904+CassiniYux@users.noreply.github.com>
2024-01-18 07:25:34 +0200

    Update E01.java

--- e01/E01.java
+++ e01/E01.java
@@ -1,39 +1,24 @@
 class E01 {
     public static void main(String[] args) {
-        
-        // Declare and initialize integer variable
+  
+        // Declare and initialize variables
         int x = 10;
-
-        // Declare and initialize floating-point variable
-        float y = 5.5f;
-
-        // Declare and initialize a boolean variable
+        float y = 5.5f;  // Use 'f' to specify float literal
         boolean flag = true;
-
-        // Declare and initialize a character variable
         char letter = 'c';
-
-        // Declare and initialize short integer variable
         short year = 2022;
-
-        // Declare and initialize long integer variable
-        long population = 7000000000L;
-
-        // Declare and initialize byte variable
+        long population = 7000000000L;  // Use 'L' to specify long literal
         byte num = 100;
-
-        // Declare and initialize double variable
         double pi = 3.13457599923384753929348;
 
-        // Print the values of the variables
-        System.out.println("x: " + x);
-        System.out.println("y: " + y);
-        System.out.println("flag: " + flag);
-        System.out.println("letter: " + letter);
-        System.out.println("year: " + year);
-        System.out.println("population: " + population);
-        System.out.println("num: " + num);
-        System.out.println("pi: " + pi);
-
+        // Print the values
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(flag);
+        System.out.println(letter);
+        System.out.println(year);
+        System.out.println(population);
+        System.out.println(num);
+        System.out.println(pi);
     }
 }
