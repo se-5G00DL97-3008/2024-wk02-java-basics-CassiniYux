3c549f5148aa7cc2f458aca2671e8fe27dc74568
Yushi Tan <119079904+CassiniYux@users.noreply.github.com>
2024-01-18 07:31:56 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -15,14 +15,14 @@ class E02 {
 
         // Perform operations
         double sum = num1 + num2;
-        double product = num1 * num2;
         double difference = num1 - num2;
+        double product = num1 * num2;
         double quotient = num1 / num2;
 
         // Print the results
         System.out.println(num1 + " + " + num2 + " = " + sum);
-        System.out.println(num1 + " * " + num2 + " = " + product);
         System.out.println(num1 + " - " + num2 + " = " + difference);
+        System.out.println(num1 + " * " + num2 + " = " + product);
         System.out.println(num1 + " / " + num2 + " = " + quotient);
 
         // Close the Scanner to prevent resource leaks
