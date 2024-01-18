c07cbe568220688bc61a389111746393d03f3ece
Yushi Tan <119079904+CassiniYux@users.noreply.github.com>
2024-01-18 08:37:42 +0200

    Update E06.java

--- e06/E06.java
+++ e06/E06.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class E06 {
     public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
 
+        // Ask the user which multiplication table to show
+        System.out.println("Which multiplication table to show?");
+        int multiplier = scanner.nextInt();
+
+        // Print the multiplication table until 10
+        for (int i = 0; i <= 10; i++) {
+            int result = i * multiplier;
+            System.out.println(i + " x " + multiplier + " = " + result);
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
